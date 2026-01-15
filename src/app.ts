b4e577194edc46a8997815635380271752d96151
ankit <ajha45075@gmail.com>
2026-01-15 12:51:36 +0530

    added auth routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { errorHandler } from './middlewares/errorHandler';
+import authRoutes from "./routes/auth.routes"
 
 const app = express();
 
 app.use(express.json());
 
+
+app.use("/auth", authRoutes)
 // Global error handler (should be after routes)
 app.use(errorHandler);
 
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,92 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
+import User, { UserRole } from "../models/User";
+import bcrypt from "bcryptjs"
 
-export const register = (req:Request, res:Response, next:NextFunction) => {
-     try{
-        
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    // 2️⃣ Check existing user
+    const existingUser = await User.findOne({ email });
 
-     }catch(error){
-        next(error)
-     }
-}
+    if (existingUser) {
+      res.status(409).json({
+        success: false,
+        message: "User already exists",
+      });
+      return;
+    }
+
+    // 3️⃣ Create user
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role: UserRole.USER,
+    });
+
+    // 4️⃣ Response (never send password)
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {  email, password } = req.body;
+    // 2️⃣ Check existing user
+    const user = await User.findOne({ email });
+    
+    if (!user) {
+      res.status(400).json({
+        success: false,
+        message: "User not found.",
+      });
+      return;
+    }
+
+    const isValidPassword = await bcrypt.compare(password,user.password)
+
+      if (!isValidPassword) {
+      res.status(400).json({
+        success: false,
+        message: "Wrong credentials.",
+      });
+      return;
+    }
+
+
+    // 4️⃣ Response (never send password)
+    res.status(200).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
